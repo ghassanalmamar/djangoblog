bc22dfd44ebb50bd03a919488233badad38654f4
Ghassan Al Mamar <Ghassan.Al.Mamar@oracle.com>
2018-10-23 17:16:53 -0700

    inline admins

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from myblog.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    empty_value_display = ''
+    inlines = [
+        CategoryInline,
+    ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    empty_value_display = ''
+    exclude = ('posts',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+
